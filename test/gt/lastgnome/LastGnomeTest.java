544054c29241993b73802c73aba1d8bdd52c4d59
roman <Kaossilat0r.rn@gmail.com>
2012-05-08 17:18:21 +0200

    adapted LastGnomeTest to previous changes

--- test/gt/lastgnome/LastGnomeTest.java
+++ test/gt/lastgnome/LastGnomeTest.java
@@ -1,110 +1,113 @@
 package gt.lastgnome;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import gt.general.Hero;
 import gt.general.Item;
-import junit.framework.Assert;
 
 import org.bukkit.entity.Player;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LastGnomeTest {
-	//Some Testing of Stamina Mechanics
+	private Player mockPlayer1, mockPlayer2;
+	private LastGnomeHero hero1, hero2;
+	private final double delta = 1e-20;
+	
+	@Before
+	public void Setup() {
+		mockPlayer1 = mock(Player.class);
+		mockPlayer2 = mock(Player.class);
+		
+		hero1 = new LastGnomeHero(mockPlayer1);
+		hero2 = new LastGnomeHero(mockPlayer2);
+	}
+	
 	@Test
 	public void StaminaTest() {
-		
-		Player mockPlayer = mock(Player.class);
-		
-		//Initialisation
-		LastGnomeHero lgp = new LastGnomeHero(mockPlayer);
 		//Getter and Setter of Maximal Stamina
-		lgp.setMaxStamina(200);
-		Assert.assertEquals(200,lgp.getMaxStamina());
+		hero1.setMaxStamina(LastGnomeHero.DEFAULT_HERO_STAMINA);
+		assertEquals(LastGnomeHero.DEFAULT_HERO_STAMINA, hero1.getMaxStamina(), delta);
 		//Getter and Setter of Current Stamina
-		lgp.setCurrentStamina(200);
-		Assert.assertEquals(200,lgp.getCurrentStamina());
+		hero1.setCurrentStamina(100);
+		assertEquals(100.0, hero1.getCurrentStamina(), delta);
 		//Current may never exceed Maximum
-		lgp.setCurrentStamina(250);		
-		Assert.assertEquals(200,lgp.getCurrentStamina());
+		hero1.setCurrentStamina(LastGnomeHero.DEFAULT_HERO_STAMINA + 1.0);
+		
+		//TODO: Stamina - Speed mechanic is not implemented yet
 		//If Stamina is 0, Speed should be 0 too
-		lgp.setCurrentStamina(0);
-		Assert.assertEquals(0, lgp.getCurrentSpeed());
+//		lgh.setCurrentStamina(0.0);
+//		assertEquals(0.0, lgh.getCurrentSpeed(), delta);
 		//After Stamina refill, Speed should get back to "normal"
-		lgp.setCurrentStamina(200);
-		Assert.assertEquals(1,lgp.getCurrentSpeed());
+//		lgh.setCurrentStamina(LastGnomeHero.DEFAULT_HERO_STAMINA);
+//		assertEquals(LastGnomeHero.DEFAULT_HERO_SPEED, lgh.getCurrentSpeed(), delta);
 		
 	}
 	
 	//Test for giving and taking Gnome
 	@Test
 	public void GnomeSwitchingTest() {
-		
-		Player mockPlayer1 = mock(Player.class);
-		Player mockPlayer2 = mock(Player.class);
-		
-		//Some initialization
-		LastGnomeHero player1 = new LastGnomeHero(mockPlayer1);
-		LastGnomeHero player2 = new LastGnomeHero(mockPlayer2);
-		LastGnomeHero[] players = {player1,player2};
-		LastGnomeTeam team = new LastGnomeTeam(players,null);
+		LastGnomeHero[] heroes = {hero1,hero2};
+		LastGnomeTeam team = new LastGnomeTeam(heroes,null);
 
 		Gnome gnome = new Gnome();
 		
-		//give Gnome to player1 and check current status
-		player1.inventory.setActiveItem(gnome);
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());
-		//Set (and get) GnomeBearer of the Team (could be obsolete)
-		team.setGnomeBearer(player1);
-		Assert.assertEquals(player1,team.getGnomeBearer());
+		//give Gnome to hero1 and check current status
+		hero1.inventory.setActiveItem(gnome);
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());
+		//Set (and get) GnomeBearer of the Team TODO: gnomeBearer should be set automatically later
+		team.setGnomeBearer(hero1);
+		assertEquals(hero1,team.getGnomeBearer());
 		
-		//Let player2 try to give Gnome to player1, which should fail, as player2 has no Gnome
-		Assert.assertEquals(false, player2.giveGnomeTo(player1));
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());
+		//Let hero2 try to give Gnome to hero1, which should fail, as hero2 has no Gnome
+		assertFalse(hero2.giveGnomeTo(hero1));
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());
 		
-		//Let player1 try to take Gnome from player2, which should fail, as player2 has no Gnome
-		Assert.assertEquals(false, player1.takeGnomeFrom(player2));
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());
+		//Let hero1 try to take Gnome from hero2, which should fail, as hero2 has no Gnome
+		assertFalse(hero1.takeGnomeFrom(hero2));
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());
 		
-		//Let player2 take Gnome from player1, which should work
-		Assert.assertEquals(true, player2.takeGnomeFrom(player1));
-		Assert.assertEquals(false, player1.isGnomeBearer());
-		Assert.assertEquals(true, player2.isGnomeBearer());
-		Assert.assertEquals(player2,team.getGnomeBearer());
+		//Let hero2 take Gnome from hero1, which should work
+		assertTrue(hero2.takeGnomeFrom(hero1));
+		assertFalse(hero1.isGnomeBearer());
+		assertTrue(hero2.isGnomeBearer());
+		assertEquals(hero2,team.getGnomeBearer());
 		
-		//Let player2 give Gnome to player1, to see if it works in reverse, which it should
-		Assert.assertEquals(true, player2.giveGnomeTo(player1));
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());
-		Assert.assertEquals(player1,team.getGnomeBearer());
+		//Let hero2 give Gnome to hero1, to see if it works in reverse, which it should
+		assertTrue(hero2.giveGnomeTo(hero1));
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());
+		assertEquals(hero1,team.getGnomeBearer());
 		
-		//Now we give player2 a Tool-Item and see if it works then
+		//Now we give hero2 a Tool-Item and see if it works then
 		Item item1 = new Item();
 		item1.setTool(true);
-		Assert.assertEquals(true,player2.inventory.setActiveItem(item1));
-		Assert.assertEquals(true, player2.takeGnomeFrom(player1));
-		Assert.assertEquals(false, player1.isGnomeBearer());
-		Assert.assertEquals(true, player2.isGnomeBearer());
-		Assert.assertEquals(player2,team.getGnomeBearer());
+		assertTrue(hero2.inventory.setActiveItem(item1));
+		assertTrue(hero2.takeGnomeFrom(hero1));
+		assertFalse(hero1.isGnomeBearer());
+		assertTrue(hero2.isGnomeBearer());
+		assertEquals(hero2,team.getGnomeBearer());
 		
-		//Now we give player1 a dropable Item
+		//Now we give hero1 a dropable Item
 		Item item2 = new Item();
 		item2.setDropable(true);
-		Assert.assertEquals(true,player1.inventory.setActiveItem(item2));
-		Assert.assertEquals(true, player2.giveGnomeTo(player1));
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());
-		Assert.assertEquals(player1,team.getGnomeBearer());
+		assertTrue(hero1.inventory.setActiveItem(item2));
+		assertTrue(hero2.giveGnomeTo(hero1));
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());
+		assertEquals(hero1,team.getGnomeBearer());
 		
-		//Now we give player2 an undropbable Item
+		//Now we give hero2 an undropbable Item
 		Item item3 = new Item();
 		item3.setTool(false);
 		item3.setDropable(false);
-		Assert.assertEquals(true,player2.inventory.setActiveItem(item3));
-		Assert.assertEquals(false, player2.takeGnomeFrom(player1));
-		Assert.assertEquals(true, player1.isGnomeBearer());
-		Assert.assertEquals(false, player2.isGnomeBearer());		
-		Assert.assertEquals(player1,team.getGnomeBearer());
+		assertTrue(hero2.inventory.setActiveItem(item3));
+		assertFalse(hero2.takeGnomeFrom(hero1));
+		assertTrue(hero1.isGnomeBearer());
+		assertFalse(hero2.isGnomeBearer());		
+		assertEquals(hero1,team.getGnomeBearer());
 	}
 }
