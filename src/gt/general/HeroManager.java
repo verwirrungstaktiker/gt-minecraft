6e12f4b7aae5ae90c4c7be78eea007cf6c1cd958
Sebastian Fahnenschreiber <sebastian@fahnenschreiber-web.de>
2012-05-18 17:49:02 +0200

    refactor

--- src/gt/general/HeroManager.java
+++ src/gt/general/HeroManager.java
@@ -16,14 +16,14 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class HeroManager implements Listener, Runnable {
 	/**Heros sorted by player*/
-	private static final Map<Player, Hero> heros = new HashMap<Player, Hero>();
+	private static final Map<Player, Hero> HEROS = new HashMap<Player, Hero>();
 	/**The plugin the manager runs in*/
 	private final JavaPlugin plugin;
 	/**
-	 * Creates a nw HeroManager
+	 * Creates a new HeroManager
 	 * @param plugin the plugin we run
 	 */
-	public HeroManager(JavaPlugin plugin) {
+	public HeroManager(final JavaPlugin plugin) {
 		this.plugin = plugin;
 
 		registerListener(this);
@@ -33,28 +33,27 @@ public class HeroManager implements Listener, Runnable {
 			.getServer()
 			.getScheduler()
 			.scheduleSyncRepeatingTask(plugin, this, 0, 1);
-
 	}
-
+	
 	/**
 	 * creates a hero for every player on login
 	 * @param ple a PlayerLoginEvent
 	 */
 	@EventHandler
-	public void playerLogin(PlayerLoginEvent ple) {
-
+	public void playerLogin(final PlayerLoginEvent ple) {
+		
 		Player player = ple.getPlayer();
-
+		
 		Hero hero = new Hero(player);
 		registerListener(hero);
 		hero.getPlayer().getInventory().setMaxStackSize(1);
-		heros.put(player, hero);
+		HEROS.put(player, hero);
 	}
 
 	/**
 	 * @param listener to be registered for all events
 	 */
-	private void registerListener(Listener listener) {
+	private void registerListener(final Listener listener) {
 		plugin.getServer()
 			  .getPluginManager()
 			  .registerEvents(listener, plugin);
@@ -63,7 +62,7 @@ public class HeroManager implements Listener, Runnable {
 	@Override
 	public void run() {
 	
-		for(Hero hero : heros.values()) {
+		for(Hero hero : HEROS.values()) {
 			hero.simulateEffects();
 		}
 
@@ -75,13 +74,16 @@ public class HeroManager implements Listener, Runnable {
 	 * @param player a Minecraft-Player
 	 * @return associated hero
 	 */
-	public static Hero getHero(Player player) {
-		return heros.get(player);
+	public static Hero getHero(final Player player) {
+		return HEROS.get(player);
 	}
 	
 	
+	/**
+	 * @return all currently online heros
+	 */
 	public static Set<Hero> getAllHeros() {
-		return new HashSet<Hero>(heros.values());
+		return new HashSet<Hero>(HEROS.values());
 	}
 	
 }
