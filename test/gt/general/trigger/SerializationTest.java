7457a3dddd5e63d7bd7994f0fb2a7a977179b1b0
Sebastian Fahnenschreiber <sebastian@fahnenschreiber-web.de>
2012-06-22 11:34:05 +0200

    tests and fixes for trigger persistance

---
+++ test/gt/general/trigger/SerializationTest.java
@@ -0,0 +1,90 @@
+package gt.general.trigger;
+
+import static gt.general.trigger.persistance.TriggerManagerPersistance.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import gt.general.trigger.persistance.TriggerManagerPersistance;
+import gt.general.trigger.response.DoorResponse;
+import gt.general.trigger.response.Response;
+import gt.plugin.listener.MultiListener;
+
+import java.util.Map;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.Listener;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MultiListener.class})
+public class SerializationTest {
+
+	TriggerManagerPersistance sut;
+	TriggerManager tm;
+	
+	
+	@Before
+	public void setup() {
+		tm = new TriggerManager();
+		sut = new TriggerManagerPersistance(tm);
+		
+		PowerMockito.mockStatic(MultiListener.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void triggerSerializes() {
+		
+		Trigger trigger = mock(Trigger.class);
+		when(trigger.getLabel()).thenReturn("trigger_1");
+		
+		TriggerContext context = new TriggerContext();
+		context.addTrigger(trigger);
+		
+		tm.addTriggerContext(context);	
+				
+		Map<String, Object> yaml = sut.asYaml();
+		
+		assertTrue(((Map<String, Object>) yaml.get(KEY_GLOBAL_CONTEXTS)).containsKey(context.getLabel()));
+		assertTrue(((Map<String, Object>) yaml.get(KEY_GLOBAL_TRIGGERS)).containsKey(trigger.getLabel()));
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void responseSerializes() {
+		
+		Response response = mock(Response.class);
+		when(response.getLabel()).thenReturn("response_1");
+		
+		TriggerContext context = new TriggerContext();
+		context.addResponse(response);
+		
+		tm.addTriggerContext(context);	
+				
+		Map<String, Object> yaml = sut.asYaml();
+		
+		assertTrue(((Map<String, Object>) yaml.get(KEY_GLOBAL_CONTEXTS)).containsKey(context.getLabel()));
+		assertTrue(((Map<String, Object>) yaml.get(KEY_GLOBAL_RESPONSES)).containsKey(response.getLabel()));
+		
+	}
+	
+	@Test
+	public void visualDumpTest() {
+		Trigger t = new PressurePlateRedstoneTrigger(mock(Block.class));
+		Response r = new DoorResponse(mock(Block.class));
+		
+		TriggerContext context = new TriggerContext();
+		context.addTrigger(t);
+		context.addResponse(r);
+		
+		tm.addTriggerContext(context);
+		
+		System.out.println(sut.toYaml());
+	}
+
+}
