ce14635ab0c955feff72f8dc319b0bf6170032aa
Sebastian Fahnenschreiber <sebastian@fahnenschreiber-web.de>
2012-07-09 14:10:38 +0200

    updated yaml serializable

--- src/gt/general/trigger/persistance/YamlSerializable.java
+++ src/gt/general/trigger/persistance/YamlSerializable.java
@@ -57,64 +57,6 @@ public abstract class YamlSerializable {
 	}
 	
 	/**
-	 * @param values mapping - e.g. received by the deserialization process
-	 * @param world where the block can be found
-	 * @return the Block
-	 */
-	public Block blockFromCoordinates(final Map<String, Object> values, final World world) {
-		return blockFromPrefixedCoordinates("", values, world);
-	}
-	
-	public Block blockFromPrefixedCoordinates(final String prefix ,final Map<String, Object> values, final World world) {
-		int x = (Integer) values.get(prefix+KEY_X_COORDINATE);
-		int y = (Integer) values.get(prefix+KEY_Y_COORDINATE);
-		int z = (Integer) values.get(prefix+KEY_Z_COORDINATE);
-		
-		return world.getBlockAt(x, y, z);
-	}
-	
-	/**
-	 * @param values mapping - e.g. received by the deserialization process
-	 * @param world where the location can be found
-	 * @return the Location
-	 */
-	public Location locationFromCoordinates(final Map<String, Object> values, final World world) {
-		return blockFromCoordinates(values, world).getLocation();
-	}	
-	
-	/**
-	 * @param block the block to be serialized
-	 * @return mapping - e.g. to be used in the serialization process
-	 */
-	public Map<String, Object> coordinatesFromBlock(final Block block) {
-		return prefixedCoordinatesFromBlock("", block);
-	}
-	
-	public Map<String, Object> prefixedCoordinatesFromBlock(final String prefix,final Block block) {
-		Map<String, Object> map = new HashMap<String,Object>();
-
-		map.put(prefix+KEY_X_COORDINATE, block.getX());
-		map.put(prefix+KEY_Y_COORDINATE, block.getY());
-		map.put(prefix+KEY_Z_COORDINATE, block.getZ());
-		
-		return map;
-	}
-	
-	/**
-	 * @param location the location to be serialized
-	 * @return mapping - e.g. to be used in the serialization process
-	 */
-	public Map<String, Object> coordinatesFromPoint(final Location location) {
-		Map<String, Object> map = new HashMap<String,Object>();
-
-		map.put(KEY_X_COORDINATE, location.getBlockX());
-		map.put(KEY_Y_COORDINATE, location.getBlockY());
-		map.put(KEY_Z_COORDINATE, location.getBlockZ());
-		
-		return map;
-	}
-	
-	/**
 	 * 
 	 * @param values map containing coordinates & orientation
 	 * @param world the minecraft world
