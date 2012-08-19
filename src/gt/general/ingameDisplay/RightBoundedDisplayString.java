a43ee4a33d1a28979d7581dfa51760d3099612f9
Sebastian Fahnenschreiber <sebastian@fahnenschreiber-web.de>
2012-08-19 21:09:20 +0200

    added missing class

---
+++ src/gt/general/ingameDisplay/RightBoundedDisplayString.java
@@ -0,0 +1,27 @@
+package gt.general.ingameDisplay;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+public class RightBoundedDisplayString extends DisplayString {
+
+	public RightBoundedDisplayString(String text, float scale, Location location) {
+		super(text, scale, location);
+	}
+	
+	
+	@Override
+	public void spawnForPlayer(final SpoutPlayer spoutPlayer) {
+
+		Location l = location.clone();
+			
+		for(String ch : text.split("")) {
+			spoutPlayer.spawnTextEntity(ch, l.clone(), scale, IngameDisplayManager.REFRESH_RATE+1, new Vector());
+			
+			l.subtract(l.getDirection().multiply(scale * 0.8f));
+		}
+	
+	}
+
+}
