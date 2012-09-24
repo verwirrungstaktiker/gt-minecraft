039ebcfab2ff8fc126dcdc7b3ba66b7eaccbe799
Sebastian Fahnenschreiber <sebastian@fahnenschreiber-web.de>
2012-09-24 21:37:26 +0200

    fixed header

--- src/gt/lastgnome/GnomeAlert.java
+++ src/gt/lastgnome/GnomeAlert.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Projektpraktikum: Game Technology 2012
+ * Minecraft-Modifikation fuer kollaboratives Spielen
+ * 
+ * Sebastian Fahnenschreiber (sebastian.fahnenschreiber@stud.tu-darmstadt.de)
+ * Roman Ness (roman.ness@stud.tu-darmstadt.de)
+ * Philipp Pascal Battenberg (philipp.battenberg@stud.tu-darmstadt.de)
+ ******************************************************************************/
 package gt.lastgnome;
 
 import gt.general.character.CharacterAttributes;
--- src/gt/plugin/meta/CustomBlockType.java
+++ src/gt/plugin/meta/CustomBlockType.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Projektpraktikum: Game Technology 2012
- * Minecraft-Modifikation f�r kollaboratives Spielen
+ * Minecraft-Modifikation fuer kollaboratives Spielen
  * 
  * Sebastian Fahnenschreiber (sebastian.fahnenschreiber@stud.tu-darmstadt.de)
- * Roman Ne� (roman.ness@stud.tu-darmstadt.de)
+ * Roman Ness (roman.ness@stud.tu-darmstadt.de)
  * Philipp Pascal Battenberg (philipp.battenberg@stud.tu-darmstadt.de)
  ******************************************************************************/
 package gt.plugin.meta;
